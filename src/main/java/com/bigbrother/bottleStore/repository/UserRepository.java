cd126c419fd0c1674d9ec27e9681a4e39a493369
elton <eltonguambe1006@gmail.com>
2024-12-31 09:16:43 +0200

    user repo

---
+++ src/main/java/com/bigbrother/bottleStore/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.bigbrother.bottleStore.repository;
+
+import com.bigbrother.bottleStore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
