8445e4e6212d8ea1d9d648db757ce379f5265e73
elton <eltonguambe1006@gmail.com>
2024-12-31 09:39:43 +0200

    Jwt service

---
+++ src/main/java/com/bigbrother/bottleStore/service/JwtService.java
@@ -0,0 +1,75 @@
+package com.bigbrother.bottleStore.service;
+
+import com.bigbrother.bottleStore.enums.ROLE;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET = "jksdhfjhjhweihuehuiroh545uwhMndnnfnjdbbhsjneeoHojejdfhjie3543jb3";
+
+
+    public String generateToken(String username, ROLE role){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 90))
+                .compact();
+    }
+
+    private Key getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    private String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public ROLE extractRole(String token){
+        Claims claims = extractAllClaims(token);
+        return ROLE.valueOf(claims.get("role", String.class));
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+}
