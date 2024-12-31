35d202074e30ee2da3783029841cf0cacd225420
elton <eltonguambe1006@gmail.com>
2024-12-31 09:22:25 +0200

    MyUserDetailsService

---
+++ src/main/java/com/bigbrother/bottleStore/service/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.bigbrother.bottleStore.service;
+
+import com.bigbrother.bottleStore.model.User;
+import com.bigbrother.bottleStore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+}
