b9de2b48481a9d09305cba5ce33368c6a8de2e04
elton <eltonguambe1006@gmail.com>
2024-12-31 09:10:12 +0200

    user dto

---
+++ src/main/java/com/bigbrother/bottleStore/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.bigbrother.bottleStore.dto;
+
+import com.bigbrother.bottleStore.enums.ROLE;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UserDTO (
+        Long id,
+        @NotNull(message = "The username is necessary")
+        @NotBlank(message = "THe user name cannot be blank")
+        String username,
+        String password,
+        String fullName,
+        @NotNull
+        @Email(message = "Enter a valid email")
+        String email,
+        String phone,
+        @NotNull(message = "Role is required")
+        ROLE role
+){
+}
