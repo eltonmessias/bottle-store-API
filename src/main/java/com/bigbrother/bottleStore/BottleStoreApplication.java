e76a4113a3fb2555f80bc33f2ffab58cb3caa109
elton <eltonguambe1006@gmail.com>
2024-12-31 08:49:56 +0200

    first commit

---
+++ src/main/java/com/bigbrother/bottleStore/BottleStoreApplication.java
@@ -0,0 +1,13 @@
+package com.bigbrother.bottleStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottleStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottleStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigbrother/bottleStore/BottleStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigbrother.bottleStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottleStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
