32767b3aff1c76e6240c5f098b2930501230b468
elton <eltonguambe1006@gmail.com>
2025-02-19 08:56:36 +0200

    feat: DatabaseException and StockUpdateException

---
+++ src/main/java/com/bigbrother/bottleStore/exceptions/StockUpdateException.java
@@ -0,0 +1,7 @@
+package com.bigbrother.bottleStore.exceptions;
+
+public class StockUpdateException extends RuntimeException {
+    public StockUpdateException(String message) {
+        super(message);
+    }
+}
