d78008d3d43d8549a17266c1a0866224aa9799ae
elton <eltonguambe1006@gmail.com>
2025-04-19 21:07:22 +0200

    feat: find all customers

--- src/main/java/com/bigbrother/bottleStore/customer/CustomerController.java
+++ src/main/java/com/bigbrother/bottleStore/customer/CustomerController.java
@@ -4,10 +4,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customer")
@@ -20,5 +19,10 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<CustomerResponse> createCustomer(@RequestBody @Valid CustomerRequest request) {
         return new ResponseEntity<>(service.createCustomer(request), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponse>> findAllCustomers() {
+        return new ResponseEntity<>(service.findAllCustomers(), HttpStatus.OK);
     }
 }
--- src/main/java/com/bigbrother/bottleStore/customer/CustomerService.java
+++ src/main/java/com/bigbrother/bottleStore/customer/CustomerService.java
@@ -5,6 +5,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
@@ -15,5 +18,9 @@ public class CustomerService {
     public CustomerResponse createCustomer(@Valid CustomerRequest request) {
         var customer = repository.save(mapper.toCustomer(request));
         return mapper.fromCustomer(customer);
+    }
+
+    public List<CustomerResponse> findAllCustomers() {
+        return repository.findAll().stream().map(mapper::fromCustomer).collect(Collectors.toList());
     }
 }
